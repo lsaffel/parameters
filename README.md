797b99cf372e2c41de1b4ec547f0ce76b66612c0
Lisa <llvvss@icloud.com>
2024-03-16 12:13:00 -0600

    default paramter for age into function declaration only, not function definition. It works

--- README.md
+++ README.md
@@ -2,4 +2,6 @@ Parameters
 Function which has one argument passed to it. It has one parameter.  
 The function is invoked more than once from main.  
 Then a function which has more than one argument passed to it.  
-It has three parameters passed to it, 2 of type string and one of type int.
+It has three parameters passed to it, 2 of type string and one of type int.  
+Put any default parameter values into the function declaration,  
+not into the function definition. i.e. age=0
--- src/parameters.cpp
+++ src/parameters.cpp
@@ -1,12 +1,14 @@
 #include<iostream>
 using namespace std;
 
-void introduceMe(string name, string city, int age);
+void introduceMe(string name, string city, int age=0);
+    // give the default value for age only here, 
+    // not in the function definition below. It gives an error otherwise.
 
 int main(){
     introduceMe("Lisa", "Calgary", 30);        // this is the argument that we pass
                         //  to our function
-    introduceMe("Anna", "Edmonton", 35);  
+    introduceMe("Anna", "Edmonton");  
 
     return 0;
 }
@@ -15,5 +17,6 @@ void introduceMe(string name, string city, int age) {         // parameters
                                   // that is passed to the function
     cout << "My name is " << name << endl;
     cout << "I am from " << city << endl;
-    cout << "I am " << age << " years old" << endl;
+    if(age!=0)
+        cout << "I am " << age << " years old" << endl;
 }
