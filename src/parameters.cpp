f40b90f10447b781a987e5fad17ce40e983c1628
Lisa <llvvss@icloud.com>
2024-03-16 12:40:17 -0600

    more

--- src/parameters.cpp
+++ src/parameters.cpp
@@ -6,10 +6,20 @@ void introduceMe(string name, string city, int age=0);
     // not in the function definition below. It gives an error otherwise.
 
 int main(){
-    introduceMe("Lisa", "Calgary", 30);        // this is the argument that we pass
-                        //  to our function
-    introduceMe("Anna", "Edmonton");  
+    // introduceMe("Lisa", "Calgary", 30);        // this is the argument that we pass
+    //                     //  to our function
+    // introduceMe("Anna", "Edmonton");  
 
+    string name1, city1;
+    int age1;
+    cout << "Name: ";
+    cin >> name1;
+    cout << "City: ";
+    cin >> city1;
+    cout << "Age: ";
+    cin >> age1;
+
+    introduceMe(name1, city1, age1);
     return 0;
 }
 
