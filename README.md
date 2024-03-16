f1baadfc7997c5edb2a13b8312fdd6bc9db65f6c
Lisa <llvvss@icloud.com>
2024-03-16 11:44:52 -0600

    multiple parameters passed to function, Readme edited

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ Parameters
 Function which has one argument passed to it. It has one parameter.  
 The function is invoked more than once from main.  
 Then a function which has more than one argument passed to it.  
-It has multiple parameters.
+It has three parameters passed to it, 2 of type string and one of type int.
--- src/parameters.cpp
+++ src/parameters.cpp
@@ -1,17 +1,19 @@
 #include<iostream>
 using namespace std;
 
-void introduceMe(string name);
+void introduceMe(string name, string city, int age);
 
 int main(){
-    introduceMe("Lisa");        // this is the argument that we pass
+    introduceMe("Lisa", "Calgary", 30);        // this is the argument that we pass
                         //  to our function
-    introduceMe("Anna");  
+    introduceMe("Anna", "Edmonton", 35);  
 
     return 0;
 }
 
-void introduceMe(string name) {         // name is the parameter 
+void introduceMe(string name, string city, int age) {         // parameters
                                   // that is passed to the function
     cout << "My name is " << name << endl;
+    cout << "I am from " << city << endl;
+    cout << "I am " << age << " years old" << endl;
 }
